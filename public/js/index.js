5ef748bcc068019518048626123db53cd97e2c9e
Basem Jaber <basemjaber32@gmail.com>
2019-12-07 12:25:29 -0700

    Updated quieries

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,18 @@
 // stuff
+$(document).ready(function(){
+
+let chatRoom = document.getElementById("page-info").className;
+
+
+function getPosts(chatRoom){
+$.get(`api/posts/${chatRoom}`,function(data){
+console.log(data);
+
+
+})
+
+}
+
+
+
+})
